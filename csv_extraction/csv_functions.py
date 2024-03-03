07fa7f5b87d81e3100e3f3c84362b474c578cb8a
Pritom-Paul <97555352+Pritom-Paul@users.noreply.github.com>
2024-03-03 15:56:32 +0600

    csv_functions.py

--- csv_extraction/csv_functions.py
+++ csv_extraction/csv_functions.py
@@ -3,7 +3,11 @@
 import pandas as pd
 import os
 
+import pandas as pd
+import os
+
 def save_csv(df, filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     output_path = os.path.join(current_dir, filename)
     df.to_csv(output_path, index=False)
+    print(f"The CSV file has been saved in the csv_extraction directory as '{filename}'")
