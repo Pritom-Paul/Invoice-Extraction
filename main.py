5ad7a9513d8d611189a0fb4e1b3c6bd808834f38
Pritom-Paul <p.paulprem@gmail.com>
2024-03-26 00:36:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ import pandas as pd
 if __name__ == "__main__":
     directory = r'C:\Users\pc\Desktop\Invoice-Extraction\Invoices'
 
-    all_invoice_data = extract_pdf_data(directory)
-    df = pd.DataFrame(all_invoice_data)
+    try:
+        all_invoice_data = extract_pdf_data(directory)
+        df = pd.DataFrame(all_invoice_data)
 
-    exporter_refs_df = extract_exporter_refs(directory)
+        exporter_refs_df = extract_exporter_refs(directory)
 
-    df['Exporters Ref'] = exporter_refs_df['Exporters Ref']
+        df['Exporters Ref'] = exporter_refs_df['Exporters Ref']
 
-    # Print DataFrame to CSV file in the same directory
-    save_excel(df, 'invoice_data')
+        # Print DataFrame to CSV file in the same directory
+        save_excel(df, 'invoice_data')
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
