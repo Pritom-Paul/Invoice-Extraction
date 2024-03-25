fba99144d4a2577c881a83857dc6e0dcbf82b74c
Pritom-Paul <97555352+Pritom-Paul@users.noreply.github.com>
2024-03-26 00:15:38 +0600

    csv_functions.py

--- csv_extraction/csv_functions.py
+++ csv_extraction/csv_functions.py
@@ -3,11 +3,9 @@
 import pandas as pd
 import os
 
-import pandas as pd
-import os
-
-def save_csv(df, filename):
+def save_excel(df, filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    output_path = os.path.join(current_dir, filename)
-    df.to_csv(output_path, index=False)
-    print(f"The CSV file has been saved in the csv_extraction directory as '{filename}'")
+    output_path = os.path.join(current_dir, f'{filename}.xlsx')
+    df.to_excel(output_path, index=False)
+    print(f"The Excel file has been saved in the csv_extraction directory as '{filename}.xlsx'")
+
