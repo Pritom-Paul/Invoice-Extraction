7dd23f8f0bc2c79521098213d1e4367037d23dc7
Pritom-Paul <97555352+Pritom-Paul@users.noreply.github.com>
2024-03-28 15:27:05 +0600

    Add files via upload

--- pdf_extraction/extract_invoice_dates.py
+++ pdf_extraction/extract_invoice_dates.py
@@ -1,14 +1,18 @@
 # pdf_extraction/extract_invoice_dates.py
 
 import re
+from datetime import datetime
 
 def extract_invoice_dates(text):
     pattern = re.findall(r'.Date:\s+(\d{2}-\d{2}-\d{4}|\d{4}-\d{2}-\d{2})', text)
 
     invoice_dates = pattern
     if invoice_dates:
-        return invoice_dates[0]
+        date_str = invoice_dates[0]
+        try:
+            date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        except ValueError:
+            date_obj = datetime.strptime(date_str, '%d-%m-%Y')
+        return date_obj.strftime('%d-%m-%Y')
     else:
         return None
-
-
